d999c1aeef2b044fd3c279851d86451a83856715
nwh1124 <nwh0716@gmail.com>
2020-12-09 21:46:25 +0900

    java html 작성 header에 css.app 적용

--- src/com/sbs/example/crawling/crawling.java
+++ src/com/sbs/example/crawling/crawling.java
@@ -42,7 +42,7 @@ public class crawling {
 		
 		List<WebElement> elements = driver.findElements(By.cssSelector(".table_list>tbody>.view"));
 		
-		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "exam");
+//		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "exam");
 		
 		for(WebElement element : elements) {			
 			
@@ -62,23 +62,23 @@ public class crawling {
 			System.out.println(hit);
 			System.out.println(recommend);
 
-			SecSql sql = new SecSql();
-			
-			sql.append("INSERT INTO article");
-			sql.append("SET regDate = ?", regDate.replaceAll("/", "-"));
-			sql.append(", updateDate = ?", updateDate.replaceAll("/", "-"));
-			sql.append(", title = ?", title);
-			sql.append(", body = ?", name);
-			sql.append(", hit = ?", hit);
-			sql.append(", recommend = ?", recommend);
-			sql.append(", memberId = ?", 1);
-			sql.append(", boardId = ?", 1);
-			
-			MysqlUtil.insert(sql);
+//			SecSql sql = new SecSql();
+//			
+//			sql.append("INSERT INTO article");
+//			sql.append("SET regDate = ?", regDate.replaceAll("/", "-"));
+//			sql.append(", updateDate = ?", updateDate.replaceAll("/", "-"));
+//			sql.append(", title = ?", title);
+//			sql.append(", body = ?", name);
+//			sql.append(", hit = ?", hit);
+//			sql.append(", recommend = ?", recommend);
+//			sql.append(", memberId = ?", 2);
+//			sql.append(", boardId = ?", 2);
+//			
+//			MysqlUtil.insert(sql);
 			
 		}
 		
-		MysqlUtil.closeConnection();
+//		MysqlUtil.closeConnection();
 		
 	}
 
--- src/com/sbs/example/textBoard/util/Util.java
+++ src/com/sbs/example/textBoard/util/Util.java
@@ -1,9 +1,14 @@
 package com.sbs.example.textBoard.util;
 
 import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Util {
 	
@@ -31,6 +36,60 @@ public class Util {
 			}
 		}
 		
+	}
+
+	public static boolean copy(String sourcePath, String targetPath) {
+		
+		Path source = Paths.get(sourcePath);
+		
+		Path target = Paths.get(targetPath);
+		
+		if(!Files.exists(target.getParent())) {
+			
+			try {
+				Files.createDirectories(target.getParent());
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		
+		try {
+			Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return true;
+		}
+		
+		return true;
+		
+	}
+
+	public static String getFileContents(String filePath) {
+		
+		String rs = null;
+		
+		FileInputStream fileStream = null;
+		
+		try {
+			
+			fileStream = new FileInputStream(filePath);
+			byte[] readBuffer = new byte[fileStream.available()];
+			
+			while(fileStream.read(readBuffer) != -1) {			
+			}
+			
+			rs = new String(readBuffer);
+			
+			fileStream.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+						
+		return rs;
 	}
 
 }
